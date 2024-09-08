0e2267cada587f02a2702bda7c0066b7aacba3d8
Anuththara93 <136674526+Anuththara93@users.noreply.github.com>
2024-09-09 00:27:01 +0530

    minor changes

--- client/src/pages/customer/Footer.jsx
+++ client/src/pages/customer/Footer.jsx
@@ -1,12 +1,42 @@
-import "../footer/footer.css"
+import React from 'react'
+import './footer.css'
+
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <span>GEAR</span>
-      <span>© GEAR Admin Dashboard</span>
+    <div className='footerr'>
+      <div className='footermain'>
+        <div className='footerdiv'>
+          <h2 className='footerh2'>Main Links</h2>
+          <ul className='footerul'>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Products</li>
+          </ul>
+        </div>
+        <div className='footerdiv'>
+          <h2 className='footerh2cd'>Contact Details</h2>
+          <div className='divl'>
+            <div className='footeri'><p>+94766516238</p></div>
+            <div className='footeri'><p>contact@gear.com</p></div>
+          </div>
+        </div>
+        <div className='footerdiv'>
+          <h2 className='footerh2'>Stores</h2>
+          <ul className='footerul'>
+            <li>Gampaha</li>
+            <li>Colombo 5</li>
+            <li>Galle</li>
+          </ul>
+        </div>
+      </div>
+      <div className='footerc'>
+        <p>Copyright © 2023 GEAR</p>
+      </div>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
+
