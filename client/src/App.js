bee0bb09bcd1e98f67efaae1dfbf9fcb604315e8
Anuththara93 <136674526+Anuththara93@users.noreply.github.com>
2024-09-08 11:51:48 +0530

    update App.js & update file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  React,
+  Router,
+  Routes,
+  Route,
+  useLocation,
+  HHome,
+  useNavigate,
+  useCookies,
+  AccessDenied,
+  Home,
+  Register,
+  Login,
+  Aboutus,
+  Navbar,
+  Contactus,
+  UpdateProfile,
+  Admin,
+  AdminLogin,
+  Orders
+} from './AppI'
+import Problems from './pages/admin/pages/problems/problems';
+import Users from './pages/admin/pages/users/users';
+import { Update } from './pages/customer/Update';
+import Workers from './pages/admin/pages/workers/workers';
+import Products from './pages/admin/pages/products/products';
+import { useEffect, useState } from "react"
+import ProductView from './pages/customer/productView';
 
 function App() {
+  const [cookies, setCookies] = useCookies(["admin_token", "access_token"]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+
+        <Routes>
+          <Route path='/' element={<HHome />} >
+            <Route path='/' element={<Home />} />
+            <Route path='/register' element={<Userverify><Register /></Userverify>} ></Route>
+            <Route path='/login' element={<Userverify><Login /></Userverify>} ></Route>
+            <Route path='/aboutus' element={<Aboutus />} />
+            <Route path='/contactus' element={<Contactus />} />
+            <Route path='/updateprofile' element={<UpdateProfile />} >
+              <Route path='/updateprofile/' element={<Update />} />
+            </Route>
+            <Route path='/productview' element={<ProductView />} />
+          </Route >
+          <Route path='/admin' element={<Adminverify><AdminLogin /></Adminverify>}> </Route>
+          <Route path='/adminlogged' element={<Adminverify2><Admin /></Adminverify2>}>
+            <Route path='/adminlogged/' element={<Adminverify><Users /></Adminverify>}> </Route>
+            <Route path='/adminlogged/products' element={<Products />} />
+            <Route path='/adminlogged/problems' element={<Problems />} />
+            <Route path='/adminloggedorders' element={<Orders />}> </Route>
+            <Route path='/adminlogged/workers' element={<Workers />}></Route>
+          </Route>
+
+        </Routes>
+      </Router >
+
+    </div >
   );
 }
 
