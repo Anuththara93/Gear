9b5284ca9ace047d811d65a6661a9079d142b34f
lakshika chandrarathna <ulakshika.c1218@gmail.com>
2024-09-08 19:51:00 +0530

    Your commit message

---
+++ client/src/pages/admin/pages/orders/orders.jsx
@@ -0,0 +1,45 @@
+import DataTable from "../../components/dataTable/dataTable";
+import "./orders.css";
+import { useState } from "react";
+import { orders } from "../../data";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "customerId",
+    type: "integer",
+    headerName: "Customer Id",
+    width: 130,
+  },
+  {
+    field: "orderDescription",
+    type: "string",
+    headerName: "Order Description",
+    width: 400,
+  },
+  {
+    field: "totalCost",
+    type: "integer",
+    headerName: "Total Cost",
+    width: 150,
+  },
+  {
+    field: "deliveryAddress",
+    headerName: "Delivery Address",
+    width: 150,
+    type: "string",
+  },
+];
+
+const Orders = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="orders">
+      Order
+      
+    </div>
+  );
+};
+
+export default Orders;
